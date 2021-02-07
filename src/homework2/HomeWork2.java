36098bb6d3d78a34f348be612c47acf46f026a65
olegilminsky <olegilminsky@yandex.ru>
2021-02-08 01:35:27 +0300

    Добавлены классы исключений MyArraySizeException и MyArrayDataException В методе main добавлен вызов метода суммирования элементов массива, обработаны исключения Выполнения ДЗ завершено

--- src/homework2/HomeWork2.java
+++ src/homework2/HomeWork2.java
@@ -1,6 +1,8 @@
 package homework2;
 
 public class HomeWork2 {
+    static int SIZE = 4;
+
     public static void main(String[] args) {
         String[][] strArray = {
                 {"1", "2", "3", "4"},
@@ -11,27 +13,21 @@ public class HomeWork2 {
 
         try {
             System.out.println(sumArrayElements(strArray));
-        } catch (MyArraySizeException e) {
-
-        } catch (MyArrayDataException e) {
-
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
         }
-
     }
 
     public static int sumArrayElements(String[][] strArray) throws MyArraySizeException, MyArrayDataException {
         int sum = 0;
-        if (strArray.length != 4) {
+        if (strArray.length != SIZE) {
             throw new MyArraySizeException("Размер массива не соответствует заданным требованиям (4х4)");
         }
         for (int i = 0; i < strArray.length; i++) {
-            if (strArray.length != 4) {
-                throw new MyArraySizeException("Размер массива не соответствует заданным требованиям (4х4)");
-            }
             for (int j = 0; j < strArray[i].length; j++) {
-                if (strArray[i].length != 4) {
+                if (strArray[i].length != SIZE) {
                     throw new MyArraySizeException("Размер массива не соответствует заданным требованиям (4х4)");
-                } else if (!strArray[i][j].matches("[0-9]+")) {
+                } else if (!strArray[i][j].matches("^[-+]?[0-9]+")) {
                     throw new MyArrayDataException(i, j);
                 } else {
                     sum += Integer.parseInt(strArray[i][j]);
